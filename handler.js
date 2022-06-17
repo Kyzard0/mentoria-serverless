0019f0ef31ed934d3d8d3d64ac73eaf81f516021
MatheusGodinho <matheus.godinho@outlook.com>
2022-06-17 15:11:55 -0300

    feat: create sqs and triggering a lambda

--- handler.js
+++ handler.js
@@ -1,4 +1,70 @@
 'use strict';
+const aws = require('aws-sdk');
+
+const validObject = ({ name, email, caps}) => {
+  return !!name && !!email && !!caps;
+};
+
+module.exports.create = async (event) => {
+  if (!validObject(event)) {
+    return {
+      statusCode: 400,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
+      body: JSON.stringify({ message: 'Missing required keys'}),
+    };
+  }
+
+  const params = {
+    DelaySeconds: 10,
+    MessageBody: JSON.stringify(event),
+    QueueUrl: proccess.env.QUEUE_URL,
+  };
+
+  const sqs = new AWS.SQS();
+
+  sqs.sendMessage(params, (err) => {
+    if (err) {
+      return {
+        statusCode: 400,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+        },
+        body: JSON.stringify({ message: 'Invalid configuration to trigger events'}),
+      };
+    } else {
+      return {
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+        },
+        body: JSON.stringify({ message: 'Message scheduled with success'}),
+      };
+    }
+  });
+};
+
+module.exports.trigger = async (event) => {
+  const params = {
+    stateMachineArn: proccess.env.STATE_MACHINE_ARN,
+    input: JSON.stringify(event),
+  };
+
+  const stepFunction = new aws.StepFunctions();
+
+  stepFunction.startExecution(params, (err) => {
+    if (err) {
+      return {
+        success: false,
+      };
+    } else {
+      return {
+        success: true,
+      };
+    }
+  });
+};
 
 module.exports.parser = async (event) => {
   const blockingCaps = ['mentoria', 'labs', 'artigo'];
