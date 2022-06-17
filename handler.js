32b726f0c54205072e56aaff8ac7e46f11198dc0
MatheusGodinho <matheus.godinho@outlook.com>
2022-06-17 18:33:54 -0300

    fix: several fixes to work sqs and step functions integration

--- handler.js
+++ handler.js
@@ -1,79 +1,103 @@
 'use strict';
-const aws = require('aws-sdk');
+const AWS = require('aws-sdk');
 
 const validObject = ({ name, email, caps}) => {
   return !!name && !!email && !!caps;
 };
 
-module.exports.create = async (event) => {
-  const { body } = event;
-  if (!validObject(body)) {
-    return {
-      statusCode: 400,
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-      },
-      body: JSON.stringify({ message: 'Missing required keys'}),
-    };
-  }
-
-  const params = {
-    DelaySeconds: 10,
-    MessageBody: JSON.stringify(body),
-    QueueUrl: proccess.env.QUEUE_URL,
+const returnResponse = ({ status, body }) => {
+  return {
+    statusCode: status,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+    body: JSON.stringify(body),
   };
+};
 
+const sendMessageToSQS = (params) => {
   const sqs = new AWS.SQS();
-
-  sqs.sendMessage(params, (err) => {
-    if (err) {
-      return {
-        statusCode: 400,
-        headers: {
-          'Access-Control-Allow-Origin': '*',
-        },
-        body: JSON.stringify({ message: 'Invalid configuration to trigger events'}),
-      };
-    } else {
-      return {
-        statusCode: 200,
-        headers: {
-          'Access-Control-Allow-Origin': '*',
-        },
-        body: JSON.stringify({ message: 'Message scheduled with success'}),
-      };
-    }
+  return new Promise((resolve, reject) => {
+    sqs.sendMessage(params, (err) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve(true);
+    });
   });
 };
 
-module.exports.trigger = async (event) => {
-  const params = {
-    stateMachineArn: proccess.env.STATE_MACHINE_ARN,
-    input: JSON.stringify(event),
-  };
-
-  const stepFunction = new aws.StepFunctions();
-
-  stepFunction.startExecution(params, (err) => {
-    if (err) {
-      return {
-        success: false,
-      };
-    } else {
-      return {
-        success: true,
-      };
-    }
+const startStepExecution = (params) => {
+  const stepFunction = new AWS.StepFunctions();
+  return new Promise((resolve, reject) => {
+    stepFunction.startExecution(params, (err) => {
+      if (err) {
+        reject(err);
+        return;
+      } else {
+        resolve(true);
+      }
+    });
   });
+};
+
+module.exports.create = async ({ body }) => {
+  try {
+    const data = JSON.parse(body);
+    if (!validObject(data)) {
+      return returnResponse({
+        status: 400,
+        body: { message: 'Missing required keys'},
+      });
+    }
+
+    const params = {
+      DelaySeconds: 10,
+      MessageBody: JSON.stringify(body),
+      QueueUrl: process.env.QUEUE_URL,
+    };
+
+    await sendMessageToSQS(params);
+
+    return returnResponse({
+      status: 200,
+      body: { message: 'Message scheduled with success'},
+    });
+  } catch (error) {
+    console.log(error);
+    return returnResponse({
+      status: 400,
+      body: { message: 'Unnexpected error' },
+    });
+  }
+};
+
+module.exports.trigger = async (event) => {
+  const records = event.Records.map(r => JSON.parse(r.body));
+
+  await Promise.all(
+    records.map(body => {
+      const params = {
+        stateMachineArn: process.env.STATE_MACHINE_ARN,
+        input: JSON.stringify(body),
+      };
+      return startStepExecution(params);
+    }),
+  );
+
+  return true;
 };
 
 module.exports.parser = async (event) => {
   const blockingCaps = ['mentoria', 'labs', 'artigo'];
-
+  const validCaps = ['palestra', 'workshop']
+  const data = JSON.parse(event);
+  const { name, email, caps} = data;
   return {
-    name: event.name,
-    email: event.email,
-    valid: blockingCaps.indexOf(event.caps) === -1,
+    name,
+    email,
+    valid: blockingCaps.indexOf(caps) === -1 && validCaps.indexOf(caps) >= 0,
   };
 };
 
